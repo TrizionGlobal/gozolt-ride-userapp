dca62ccb31eaac9f043a29866551dd626eb1540e
VadlaBhanuPrasad <bhanuvadla88@gmail.com>
2026-05-08 18:45:07 +0530

    feat: integrate Firebase and Google Services into the Android build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -74,6 +74,6 @@ flutter {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-analytics")
 }
